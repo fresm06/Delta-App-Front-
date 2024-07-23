1a6214eb566b26168fcbdd2f88a93fbfbdd3d7e8
lear1091 <110961566+lear1091@users.noreply.github.com>
2024-07-23 10:25:42 +0900

    Main Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,198 @@
 import 'package:flutter/material.dart';
-import 'intro_screen.dart';
-import 'MyHomePage.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FutureBuilder(
-        future: Future.delayed(const Duration(seconds: 2), () => "Completed."),
-        builder: (context, snapshot) {
-          return AnimatedSwitcher(
-            duration: const Duration(milliseconds: 1000),
-            child: _splashLoadingWidget(snapshot)
-          );
-        },
-      )
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
     );
   }
 }
 
-Widget _splashLoadingWidget(AsyncSnapshot<Object?> snapshot) {
-  if(snapshot.hasError) {
-    return const Text("Error!!");
-  } else if(snapshot.hasData) {
-    return const MyHomePage();
-  } else {
-    return const IntroScreen();
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.close)),
+        ],
+        leading: Container(
+          width: 30,
+          height: 30,
+          padding: const EdgeInsets.all(10),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('Delta_image.png'),
+            ),
+          ),
+        ),
+        title: const Text('Delta', style: TextStyle(fontFamily: 'moya')),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Column(
+              children: [
+                Text('ID: 사용자', style: TextStyle(fontSize: 20)),
+                Text('e-mail', style: TextStyle(fontSize: 20)),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 16, top: 16),
+            child: const Example1Page(),
+          ),
+          const SizedBox(height: 8), // Adding a small gap between Example1Page and MyWidget
+          Expanded(child: MyWidget()),
+        ],
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.vertical,
+      children: <Widget>[
+        ListTile(
+          leading: const Icon(Icons.book),
+          title: const Text('가장 잘하는 과목'),
+          trailing: const Icon(Icons.navigate_next),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: const Icon(Icons.language),
+          title: const Text('언어'),
+          trailing: const Icon(Icons.navigate_next),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: const Icon(Icons.person_2_outlined),
+          title: const Text('프로필 관리'),
+          trailing: const Icon(Icons.navigate_next),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: const Icon(Icons.settings),
+          title: const Text('설정'),
+          trailing: const Icon(Icons.navigate_next),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: const Icon(Icons.alarm),
+          title: const Text('푸시 알람'),
+          trailing: const Icon(Icons.navigate_next),
+          onTap: () {},
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            width: 100,
+            height: 50,
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(15),
+            child: const Text(
+              '로그아웃',
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: Colors.red,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Example1Page extends StatefulWidget {
+  const Example1Page({Key? key}) : super(key: key);
+
+  @override
+  State<Example1Page> createState() => _Example1PageState();
+}
+
+class _Example1PageState extends State<Example1Page> {
+  @override
+  Widget build(BuildContext context) {
+    final double _imageSize = MediaQuery.of(context).size.width / 4;
+
+    return Container(
+      margin: const EdgeInsets.only(left: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            constraints: BoxConstraints(
+              minHeight: _imageSize,
+              minWidth: _imageSize,
+            ),
+            child: GestureDetector(
+              onTap: () {
+                _showBottomSheet();
+              },
+              child: const Center(
+                child: Icon(
+                  Icons.account_circle,
+                  size: 80,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(25),
+        ),
+      ),
+      builder: (context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('사진찍기'),
+            ),
+            SizedBox(height: 10),
+            const Divider(
+              thickness: 3,
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('사진첩에서 불러오기'),
+            ),
+            SizedBox(height: 20),
+          ],
+        );
+      },
+    );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:delta/main.dart';
+import 'package:contact/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
